217786e0524878198b1d683d2ce2c334da09eb2f
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:50:52 +0700

    Create QueryResult.php

---
+++ src/QueryResult.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm;
+
+use \PDOStatement;
+use Evas\Orm\Base\Database;
+use Evas\Orm\Base\QueryResult as BaseQueryResult;
+
+/**
+ * Класс-обертка базового QueryResult с добавление IdentityMap.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class QueryResult extends BaseQueryResult
+{
+    /**
+     * @var Database
+     */
+    protected $database;
+
+    /**
+     * Конструктор.
+     * @param PDOStatement
+     */
+    public function __construct(PDOStatement &$stmt, Database &$database)
+    {
+        $this->stmt = &$stmt;
+        $this->database = $database;
+    }
+
+    /**
+     * Переопределяем хук для постобработки полученного объекта записи.
+     * @param object|null запись
+     * @return object|null постобработанная запись
+     */
+    public function objectHook(object $row = null)
+    {
+        if (!empty($row) && is_object($row)) {
+            $row = $this->identityMapHookUpdate($row);
+        }
+        return $row;
+    }
+
+    /**
+     * Переопределяем хук для постобработки полученных объектов записей.
+     * @param array|null записи
+     * @return array|null постобработанные записи
+     */
+    public function objectsHook(array $rows = null): ?array
+    {
+        if (!empty($rows)) foreach ($rows as &$row) {
+            $row = $this->objectHook($row);
+        }
+        return $rows;
+    }
+
+
+    // Добавленные хуки
+
+    /**
+     * Хук для обновления сущности в IdentityMap.
+     * @param object
+     */
+    public function identityMapHookUpdate(object &$object)
+    {
+        $table = $this->tableName();
+        $primaryKey = $this->database->table($table)->primaryKey();
+        return $this->database->identityMapUpdate($object, $primaryKey);
+    }
+}
