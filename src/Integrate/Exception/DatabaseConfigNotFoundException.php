4eb647d0fea55300e8cad6691bc5902f6ee77a01
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:45:37 +0700

    Create DatabaseConfigNotFoundException.php

---
+++ src/Integrate/Exception/DatabaseConfigNotFoundException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Integrate\Exception;
+
+use \Throwable;
+use Evas\Orm\OrmException;
+
+/**
+ * Класс исключения ненайденного конфига базы данных.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class DatabaseConfigNotFoundException extends OrmException
+{
+    /**
+     * Переопределяем базовый конструтор исключения.
+     * @param string имя базы данных
+     * @param int код
+     * @param Throwable предыдущие исключения
+     */
+    public function __construct(string $name = null, int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct("Database config \"$name\" not found", $code, $previous);
+    }
+}
