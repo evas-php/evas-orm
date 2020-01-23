e2785ae8dbfc63a147de0aa2ace1dccfbd07ccaf
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:43:49 +0700

    Create InsertBuilderEmptyRowsException.php

---
+++ src/Builders/Exception/InsertBuilderEmptyRowsException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Builders\Exception;
+
+use Evas\Orm\OrmException;
+
+/**
+ * Класс исключения отсутствия записей в сборщике INSERT-запроса.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class InsertBuilderEmptyRowsException extends OrmException
+{}
