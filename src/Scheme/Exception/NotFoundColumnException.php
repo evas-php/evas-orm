25f0b4ed86e3ff36e38ae7469b4e9f4a0ee6d5f0
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:47:59 +0700

    Create NotFoundColumnException.php

---
+++ src/Scheme/Exception/NotFoundColumnException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Scheme\Exception;
+
+use Evas\Orm\OrmException;
+
+/**
+ * Класс исключения обращения к несуществующей колонке таблицы.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class NotFoundColumnException extends OrmException
+{}
