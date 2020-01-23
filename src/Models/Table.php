2b7b6586a37dd022920efaf54590fbc379f27087
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:48:19 +0700

    Create Table.php

---
+++ src/Models/Table.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Models;
+
+use Evas\Orm\Base\QueryResult;
+use Evas\Orm\Builders\QueryBuilder;
+use Evas\Orm\Scheme\TableScheme;
+
+/**
+ * Класс таблицы базы данных.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Table extends TableScheme
+{
+    // Table CRUD
+
+    /**
+     * Начало сборки INSERT-запроса.
+     * @param array|object|null значения записи для сохранения с автосборкой
+     * @return InsertBuilder|QueryResult
+     */
+    public function insert($row = null)
+    {
+        $this->database->insert($this->table, $row);
+    }
+
+    /**
+     * Вставка нескольких записей.
+     * @param array ключи записи
+     * @param array значения записей
+     * @return QueryResult
+     */
+    public function batchInsert(array $keys, $rows): QueryResult
+    {
+        return $this->database->batchInsert($this->table, $keys, $rows);
+    }
+
+    /**
+     * Начало сборки SELECT-запроса.
+     * @param string|null столбцы
+     * @return QueryBuilder
+     */
+    public function select(string $columns = null): QueryBuilder
+    {
+        return $this->database->select($this->table, $columns);
+    }
+
+    /**
+     * Начало сборки UPDATE-запроса.
+     * @param array|object значения записи
+     * @return QueryBuilder
+     */
+    public function update($row): QueryBuilder
+    {
+        return $this->database->update($this->table, $row);
+    }
+
+    /**
+     * Начало сборки DELETE-запроса.
+     * @return QueryBuilder
+     */
+    public function delete(): QueryBuilder
+    {
+        return $this->database->delete($this->table);
+    }
+
+    /**
+     * Получение id последней вставленной записи.
+     * @return int|null
+     */
+    public function lastInsertId(): ?int
+    {
+        return $this->database->lastInsertId($this->table);
+    }
+}
