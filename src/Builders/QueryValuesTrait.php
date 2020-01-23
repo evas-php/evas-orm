6822f0b6874348b6e0d1ca0ed46c4c666979fb4e
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:44:26 +0700

    Create QueryValuesTrait.php

---
+++ src/Builders/QueryValuesTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Builders;
+
+/**
+ * Трейт сборки значений запроса.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+trait QueryValuesTrait
+{
+    /**
+     * @var array значения запроса для экранирования
+     */
+    public $values = [];
+
+    /**
+     * Добавление значения.
+     * @param string|numeric алиас или значение
+     * @param string|numeric|null значение или null
+     * @return self
+     */
+    public function bindValue($name, $value = null)
+    {
+        assert(is_string($name) || is_numeric($name));
+        if (!empty($value)) {
+            assert(is_string($value) || is_numeric($value));
+            $this->values[$name] = $value;
+        } else {
+            $this->values[] = $name;
+        }
+        return $this;
+    }
+
+    /**
+     * Добавление значений.
+     * @param array массив или маппинг значений
+     * @return self
+     */
+    public function bindValues(array $values)
+    {
+        $this->values = array_merge($this->values, $values);
+        return $this;
+    }
+
+    /**
+     * Получение значений.
+     * @return array
+     */
+    public function values(): array
+    {
+        return $this->values;
+    }
+}
