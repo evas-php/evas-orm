00c0085b68aa8d22d0c19e1efdff9b5ca6788edf
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:50:54 +0700

    Create Database.php

---
+++ src/Database.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm;
+
+use Evas\Orm\Base\Database as BaseDatabase;
+use Evas\Orm\Builders\DatabaseBuildersTrait;
+
+use Evas\Orm\Models\DatabaseIdentityMapTrait;
+use Evas\Orm\Models\DatabaseTableTrait;
+use Evas\Orm\QueryResult;
+use Evas\Orm\Scheme\DatabaseScheme;
+
+
+/**
+ * Константы для свойств класс базы данных по умолчанию.
+ */
+if (!defined('EVAS_DATABASE_QUERY_RESULT_CLASS')) {
+    define('EVAS_DATABASE_QUERY_RESULT_CLASS', QueryResult::class);
+}
+
+
+/**
+ * Класс соединения с базой данных.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Database extends BaseDatabase
+{
+    /**
+     * Подключаем поддержку сборщиков запросов.
+     * Подключаем поддержку классов таблиц.
+     * Подключаем поддержку IdentityMap.
+     */
+    use DatabaseBuildersTrait, DatabaseTableTrait, DatabaseIdentityMapTrait;
+}
