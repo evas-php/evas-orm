6a7aa4fbbc57a4c9c461332c5593b3dd283f3375
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:42:32 +0700

    Create DatabaseConnectionException.php

---
+++ src/Base/Exception/DatabaseConnectionException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Base\Exception;
+
+use Evas\Orm\OrmException;
+
+/**
+ * Класс исключения соединения с базой данных.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class DatabaseConnectionException extends OrmException
+{}
