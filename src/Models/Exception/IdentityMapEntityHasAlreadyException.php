dcc4bd812c7d3d8bbc645d56f2cc0aaf1b8e94e6
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:50:03 +0700

    Create IdentityMapEntityHasAlreadyException.php

---
+++ src/Models/Exception/IdentityMapEntityHasAlreadyException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Models\Exception;
+
+use Evas\Orm\Models\Exception\IdentityMapException;
+
+/**
+ * Класс исключения наличия сущности IdentityMap.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class IdentityMapEntityHasAlreadyException extends IdentityMapException
+{}
