d491ba960c7f62472f1f6725687b249268b862a8
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:49:21 +0700

    Create IdentityMap.php

---
+++ src/Models/IdentityMap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Models;
+
+use Evas\Orm\Models\IdentityMapTrait;
+
+/**
+ * Класс IdentityMap.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class IdentityMap
+{
+    /**
+     * Подключаем трейт реализации IdentityMap.
+     */
+    use IdentityMapTrait;
+}
