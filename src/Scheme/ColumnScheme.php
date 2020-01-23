c0accd15ac24e30ed6bcbb1393854cdd9d5bc711
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:47:49 +0700

    Create ColumnScheme.php

---
+++ src/Scheme/ColumnScheme.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Scheme;
+
+/**
+ * Класс схемы колонки.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class ColumnScheme
+{
+    /**
+     * @var string имя
+     */
+    public $name;
+
+    /**
+     * @var string тип
+     */
+    public $type;
+
+    /**
+     * @var bool разрешен NULL
+     */
+    public $null;
+
+    /**
+     * @var string ключ
+     */
+    public $key;
+
+    /**
+     * @var string значение по умолчанию
+     */
+    public $default;
+
+    /**
+     * @var string extra
+     */
+    public $extra;
+
+    /**
+     * Конструктор.
+     * @param array параметры колонки
+     */
+    public function __construct(array $params = null)
+    {
+        if ($params) foreach ($params as $name => $value) {
+            $this->__set($name, $value);
+        }
+    }
+
+    public function __set(string $name, $value)
+    {
+        if ('Field' === $name) $name = 'name';
+        else if ('Null' === $name) $value = 'YES' == $value ? true : false;
+        $name = strtolower($name);
+        $this->$name = $value;
+    }
+
+    /**
+     * Проверка на индекс.
+     * @return bool
+     */
+    public function isIndex(): bool
+    {
+        return !empty($this->key) ? true : false;
+    }
+}
