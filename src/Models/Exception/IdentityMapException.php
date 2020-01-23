9b3c85bcb0dbc5394dd66e4e6f12f21a010e54e9
Egor Vasyakin <evasyakin@mail.ru>
2020-01-24 00:49:35 +0700

    Create IdentityMapException.php

---
+++ src/Models/Exception/IdentityMapException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-orm
+ */
+namespace Evas\Orm\Models\Exception;
+
+use Evas\Orm\OrmException;
+
+/**
+ * Общий класс исключений IdentityMap.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class IdentityMapException extends OrmException
+{}
