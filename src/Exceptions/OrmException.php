130be3f2b45a0f6015b6089b01200e14b4e868ff
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-05-06 03:43:59 +0300

    Create OrmException.php

---
+++ src/Exceptions/OrmException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Класс исключений модуля evas-orm.
+ * @package evas-php\evas-orm
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Orm\Exceptions;
+
+class OrmException extends \Exception
+{}
