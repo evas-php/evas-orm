5fde1f109335237f2de59315850c3b96b2cae9c1
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-02-22 20:22:15 +0300

    Update ActiveRecord.php

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -6,24 +6,36 @@
  */
 namespace Evas\Orm;
 
+use \JsonSerializable;
 use Evas\Base\App;
 use Evas\Base\Help\HooksTrait;
 use Evas\Db\Interfaces\DatabaseInterface;
-use Evas\Db\Interfaces\QueryBuilderInterface;
 use Evas\Db\Table;
 use Evas\Orm\Exceptions\LastInsertIdUndefinedException;
+use Evas\Orm\RelatedCollection;
+use Evas\Orm\Traits\RelationsTrait;
+use Evas\Orm\QueryBuilder;
 
-abstract class ActiveRecord
+abstract class ActiveRecord implements JsonSerializable
 {
     // подключаем поддержку произвольных хуков в наследуемых классах
     use HooksTrait;
 
-    /** @var string имя соединения с базой данных*/
+    // подключаем поддержку связей
+    use RelationsTrait;
+
+    // подключаем методы сборщика запросов
+    // use ActiveRecordBuilderTrait;
+
+    /** @var string имя соединения с базой данных */
     public static $dbname;
     /** @var string имя соединения с базой данных только для записи */
     public static $dbnameWrite;
-    /*** @var string кастомное имя таблицы */
+    /** @var string кастомное имя таблицы */
     public static $tableName;
+
+    /** @var array данные модели */
+    protected $modelData = [];
 
     /**
      * Получение соединения с базой данных.
@@ -54,14 +66,10 @@ abstract class ActiveRecord
      */
     public static function generateTableName(): string
     {
-        $className = get_called_class();
+        $className = static::class;
         $lastSlash = strrpos($className, '\\');
         if ($lastSlash > 0) {
             $className = substr($className, $lastSlash + 1);
-        }
-        $mapperPos = strrpos($className, 'Mapper');
-        if (false !== $mapperPos) {
-            $className = substr($className, 0, $mapperPos);
         }
         return strtolower(preg_replace('/([a-z0-9]+)([A-Z]{1})/', '$1_$2', $className)) . 's';
     }
@@ -168,6 +176,15 @@ abstract class ActiveRecord
         return $this;
     }
 
+    /**
+     * Получение значения первичного ключа модели.
+     * @return int|string|null
+     */
+    public function primaryValue()
+    {
+        return $this->{static::primaryKey()};
+    }
+
 
     /**
      * Получение маппинга данных записи для базы данных.
@@ -191,12 +208,12 @@ abstract class ActiveRecord
     public function getState(): array
     {
         $state = static::getDb()->identityMapGetState($this, static::primaryKey());
-        foreach ($state as $name => &$value) {
+        if ($state) foreach ($state as $name => &$value) {
             if (!isset($value) || !in_array($name, static::columns())) {
                 unset($state[$name]);
             }
         }
-        return $state;
+        return $state ?? [];
     }
 
     /**
@@ -206,8 +223,7 @@ abstract class ActiveRecord
     public function getUpdatedProperties(): array
     {
         $props = $this->getRowProperties();
-        $pk = static::primaryKey();
-        if (empty($this->$pk)) {
+        if (empty($this->primaryValue())) {
             return $props;
         } else {
             // $state = static::getDb()->identityMapGetState($this, $pk);
@@ -246,12 +262,26 @@ abstract class ActiveRecord
             $this->hook('afterInsert');
         } else {
             $this->hook('beforeUpdate');
-            static::getDb(true)->update(static::tableName(), $this->getUpdatedProperties())
-                ->where("$pk = ?", [$this->$pk])->one();
+            static::getDb(true)->table(static::tableName())
+                ->where($pk, $this->$pk)->update($this->getUpdatedProperties());
             $this->hook('afterUpdate');
         }
         $this->hook('afterSave');
         return static::getDb()->identityMapUpdate($this, $pk);
+    }
+
+    /**
+     * Сохранение модели и её связей.
+     */
+    public function push()
+    {
+        $this->save();
+        foreach ($this->relatedCollections as $models) {
+            if (!is_array($models)) $models = [$models];
+            foreach ($models as $model) {
+                $model->push();
+            }
+        }
     }
 
     /**
@@ -265,14 +295,24 @@ abstract class ActiveRecord
             return $this;
         }
         $this->hook('beforeDelete');
-        $qr = static::getDb(true)->delete(static::tableName())
-            ->where("$pk = ?", [$this->$pk])->one();
+        $qr = static::getDb(true)->table(static::tableName())
+            ->where($pk, $this->$pk)->limit(1)->delete();
         $this->hook('afterDelete', $qr->rowCount());
         if (0 < $qr->rowCount()) {
             static::getDb()->identityMapUnset($this, $pk);
             $this->id = null;
         }
         return $this;
+    }
+
+    /**
+     * Обновление данных модели из базы.
+     */
+    public function reload()
+    {
+        $pv = $this->primaryValue();
+        if (!$pv) return;
+        $this->fill(static::find($pv));
     }
 
     /**
@@ -296,42 +336,16 @@ abstract class ActiveRecord
     }
 
     /**
-     * Поиск записи через сборщик запроса.
-     * @param string|null столбцы
-     * @return QueryBuilderInterface
+     * Поиск записи/записей.
+     * @param array|int столбец или столбцы
+     * @return static|static[]
      */
-    public static function find(string $columns = null): QueryBuilderInterface
+    public static function find($ids)
     {
-        return static::getDb()->select(static::tableName(), $columns);
+        $ids = is_array($ids) ? $ids : func_get_args();
+        $db = static::getDb();
+        return (new QueryBuilder($db, static::class))->find($ids);
     }
-
-    /**
-     * Поиск по первичному ключу.
-     * @param int|string значение первичного ключа, перечисление
-     * @return static|array of static
-     */
-    public static function findByPK(...$ids)
-    {
-        $pk = static::primaryKey();
-        $qb = static::find();
-        if (count($ids) > 1) {
-            return $qb->whereIn("`$pk`", $ids)
-            ->query(count($ids))->classObjectAll(static::class);
-        } else {
-            return $qb->where("`$pk` = ?", $ids)
-            ->one()->classObject(static::class);
-        }
-    }
-
-    /**
-     * Поиск по id, алиас для findByPK.
-     * @param int id, перечисление
-     * @return static|array of static
-     */
-     public static function findById(int ...$ids)
-     {
-        return static::findByPK(...$ids);
-     }
 
     /**
      * Поиск по sql-запросу.
@@ -343,7 +357,256 @@ abstract class ActiveRecord
     {
         $qr = static::getDb()->query($sql, $values);
         return strstr($qr->stmt()->queryString, 'LIMIT 1') 
-            ? $qr->classObject(static::class)
-            : $qr->classObjectAll(static::class);
+            ? $qr->object(static::class)
+            : $qr->objectAll(static::class);
     }
+
+    /**
+     * Получение или создание новой записи.
+     * @param array свойства поиска
+     * @param array свойства вставки
+     * @return static
+     */
+    public static function firstOrCreate(array $findProps, array $createProps = null)
+    {
+        $keys = array_keys($findProps);
+        $vals = array_values($findProps);
+        foreach (static::columns() as $i => $key) {
+            if (!in_array($key, $keys)) {
+                unset($keys[$i]);
+                unset($vals[$i]);
+            }
+        }
+        $record = static::whereRowValues($keys, $vals)->one();
+        if ($record) return $record;
+        return static::create(array_merge($findProps, $createProps));
+    }
+
+    /**
+     * Проброс сборщика запросов через магический вызов статического метода.
+     * @param string имя метода
+     * @param array|null аргументы
+     * @return mixed результат выполнения метода
+     * @throws \BadMethodCallException
+     */
+    public static function __callStatic(string $name, array $args = null)
+    {
+        if (method_exists(QueryBuilder::class, $name)) {
+            $db = static::getDb();
+            return (new QueryBuilder($db, static::class))->$name(...$args);
+        }
+        throw new \BadMethodCallException(sprintf(
+            'Call to undefined method %s::%s()', __CLASS__, $name
+        ));
+    }
+
+    /**
+     * Описание связей модели.
+     * @return array
+     */
+    protected static function relations(): array
+    {
+        return [];
+    }
+
+    /**
+     * Подгрузка связанных записей.
+     * @param string имя связи
+     * @param array|null столбцы
+     * @return self
+     */
+    public function loadRelated(string $name, array $columns = null, QueryBuilder $query = null)
+    {
+        $relation = static::getRelation($name);
+        if ($query) {
+            $qb = $query->where($relation->foreignKey, $this->{$relation->localKey});
+        } else {
+            $qb = $relation->foreignModel::where($relation->foreignKey, $this->{$relation->localKey});
+        }
+        if ($relation->type === 'hasOne') $qb->limit(1);
+        $models = $qb->get($columns);
+        $this->setRelatedCollection($relation->name, $models);
+        return $this;
+    }
+
+    public function load(...$props)
+    {
+        foreach ($props as &$prop) {
+            if (is_string($prop)) {
+                @list($name, $columns) = explode(':', $prop);
+                $this->loadRelated($name, $columns);
+            } else if (is_array($prop)) {
+                foreach ($prop as $name => &$sub) {
+                    $columns = null;
+                    $query = null;
+                    if (is_string($name)) {
+                        if (is_string($sub)) {
+                            $columns = $sub;
+                        } else if (is_callable($sub)) {
+                            @list($name, $columns) = explode(':', $name);
+                            $relation = $this->getRelation($name);
+                            if (!$relation) continue;
+                            $db = static::getDb();
+                            $query = new QueryBuilder($db, $relation->foreignModel);
+                            $sub($query);
+                        }
+                    } else if (is_string($sub)) {
+                        @list($name, $columns) = explode(':', $sub);
+                    } else {
+                        throw new \InvalidArgumentException('Incorrect load syntax');
+                    }
+                    $this->loadRelated($name, $columns, $query);
+                }
+            }
+        }
+    }
+
+    /**
+     * Получение данных модели.
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->modelData;
+    }
+
+    /**
+     * Получение всех связанных записей модели.
+     * @return array
+     */
+    public function getRelatedCollections(): array
+    {
+        return $this->relatedCollections;
+    }
+
+    /**
+     * Получение конкретных связанных записей модели.
+     * @param string имя связи
+     * @return RelatedCollection|null
+     */
+    public function getRelatedCollection(string $name): ?RelatedCollection
+    {
+        if (static::hasRelation($name)) {
+            if (!isset($this->relatedCollections[$name])) {
+                $this->loadRelated($name);
+            }
+            return $this->relatedCollections[$name] ?? null;
+        }
+        return null;
+    }
+
+    /**
+     * Установка свойства.
+     * @param string имя
+     * @param mixed значение
+     */
+    public function __set(string $name, $value)
+    {
+        $this->modelData[$name] = $value;
+    }
+
+    /**
+     * Получение свойства или связанных моделей.
+     * @param string имя
+     * @return mixed значение
+     */
+    public function __get(string $name)
+    {
+        if (static::hasRelation($name)) {
+            return $this->getRelatedCollection($name);
+        } else {
+            return $this->modelData[$name] ?? null;
+        }
+    }
+
+    /**
+     * Проверка наличия свойства.
+     * @param string имя свойства
+     * @return bool
+     */
+    public function __isset(string $name): bool
+    {
+        return isset($this->modelData[$name]);
+    }
+
+    /**
+     * Очистка свойства.
+     * @param string имя свойства
+     */
+    public function __unset(string $name)
+    {
+        unset($this->modelData[$name]);
+    }
+
+    // Конвертация
+
+    /**
+     * Конвертация данных и связей модели в массив.
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return array_merge($this->getData(), $this->getRelatedCollections());
+    }
+    
+    /**
+     * Конвертация модели в JSON сериализацию.
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Конвертация данных и связей модели в JSON.
+     * @param int|null опции для json_encode
+     * @return string
+     */
+    public function toJson(int $options = JSON_UNESCAPED_UNICODE): string
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Конвертация модели в строку.
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toJson();
+    }
+
+    // protected static $list = [];
+    // protected static $map = [];
+
+    // public static function list(): array
+    // {
+    //     return static::$list;
+    // }
+
+    // public static function map(): array
+    // {
+    //     return static::$map;
+    // }
+
+    // public static function size(): int
+    // {
+    //     return count(static::$map);
+    // }
+
+    // public static function getCached($id = null)
+    // {
+    //     if (null === $id) {
+    //         return static::list();
+    //     } else if (is_array($id)) {
+    //         $result = [];
+    //         foreach ($id as $sub) {
+    //             $result[] = static::getCached($sub);
+    //         }
+    //         return $result;
+    //     } else {
+    //         return static::$map[$id];
+    //     }
+    // }
 }
