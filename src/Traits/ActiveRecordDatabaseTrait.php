4689c6b86853491044eac37673437998527ae61c
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-04 03:36:16 +0300

    Create ActiveRecordDatabaseTrait.php

---
+++ src/Traits/ActiveRecordDatabaseTrait.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Трейт соединения базы данных для Active Record.
+ * @package evas-php\evas-orm
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Orm\Traits;
+
+use Evas\Base\App;
+use Evas\Db\Interfaces\DatabaseInterface;
+
+trait ActiveRecordDatabaseTrait
+{
+    /** @var string имя соединения с базой данных */
+    public static $dbname;
+    /** @var string имя соединения с базой данных только для записи */
+    public static $dbnameWrite;
+
+    /**
+     * Получение соединения с базой данных.
+     * @param bool использовать ли соединение для записи
+     * @return DatabaseInterface
+     */
+    public static function getDb(bool $write = false): DatabaseInterface
+    {
+        $dbname = static::getDbName($write);
+        return App::db($dbname);
+    }
+
+    /**
+     * Получение имени соединения с базой данных.
+     * @param bool использовать ли соединение для записи
+     * @return string|null
+     */
+    public static function getDbName(bool $write = false): ?string
+    {
+        return true === $write && !empty(static::$dbnameWrite)
+            ? static::$dbnameWrite : static::$dbname;
+    }
+}
