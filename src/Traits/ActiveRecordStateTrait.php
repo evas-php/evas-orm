882520157ab9eac28be7eed82548c27079643ad7
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-04 03:36:40 +0300

    Create ActiveRecordStateTrait.php

---
+++ src/Traits/ActiveRecordStateTrait.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Трейт состояния для Active Record.
+ * @package evas-php\evas-orm
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Orm\Traits;
+
+trait ActiveRecordStateTrait
+{
+    /** @var array состояние полей модели */
+    protected $state = [];
+
+    /**
+     * Сохранение состояния.
+     * @return self
+     */
+    public function saveState()
+    {
+        $this->state = $this->getProps();
+        return $this;
+    }
+
+    /**
+     * Получение состояния сохраняемых полей.
+     * @return array
+     */
+    public function getState(): array
+    {
+        return $this->state;
+    }
+
+    /**
+     * Проверка наличия значения первичного ключа записи в состоянии.
+     * @return bool
+     */
+    public function isStateHasPrimaryValue(): bool
+    {
+        return isset($this->state[static::primaryKey()]);
+    }
+
+    /**
+     * Получение данных записи для базы данных.
+     * @return array
+     */
+    public function getProps(): array
+    {
+        $props = [];
+        foreach (static::columns() as &$column) {
+            if (isset($this->$column)) $props[$column] = $this->$column;
+        }
+        return $props;
+    }
+
+    /**
+     * Получение маппинга измененных свойств записи.
+     * @return array
+     */
+    public function getUpdatedProps(): array
+    {
+        $props = $this->getProps();
+        if (empty($this->primaryValue())) {
+            return $props;
+        } else {
+            $state = $this->getState();
+            // return array_diff($props, $state ?? []);
+            return array_merge(
+                array_fill_keys(array_keys(array_diff($state ?? [], $props)), null),
+                array_diff_assoc($props, $state ?? [])
+            );
+        }
+    }
+}
