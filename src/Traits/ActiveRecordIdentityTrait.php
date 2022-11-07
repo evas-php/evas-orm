28150e550b8f607dcb163d87e6ab9a121b8d9c02
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-08 02:13:01 +0300

    Update ActiveRecordIdentityTrait.php

--- src/Traits/ActiveRecordIdentityTrait.php
+++ src/Traits/ActiveRecordIdentityTrait.php
@@ -6,27 +6,38 @@
  */
 namespace Evas\Orm\Traits;
 
-use Evas\Orm\Identity\ModelIdentity;
+use Evas\Orm\ActiveRecord;
 use Evas\Orm\IdentityMap;
 
 trait ActiveRecordIdentityTrait
 {
+    /** @var string идентификатор модели для IdentityMap */
     protected $identity;
 
-    public function identity()
+    /** 
+     * Получение идентификатора модели для IdentityMap 
+     * @return string идентификатор модели для IdentityMap 
+     * */
+    public function identity(): string
     {
         if (!$this->identity) {
-            // $this->identity = ModelIdentity::createFromModel($this);
             $this->identity = implode(':', [static::class, $this->primaryValue(), static::getDbName()]);
         }
         return $this->identity;
     }
 
-    public function identityMapSave()
+    /**
+     * Сохранение модели в IdentityMap.
+     * @return static
+     */
+    public function identityMapSave(): ActiveRecord
     {
-        return IdentityMap::getOrSet($this);
+        return IdentityMap::getWithSave($this);
     }
 
+    /**
+     * Удаление модели из IdentityMap.
+     */
     public function identityMapRemove()
     {
         IdentityMap::unset($this);
