63ec9ca57484953e30c7215bb3ca2cede22fcd67
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-11-21 01:29:39 +0300

    Create RelatedCollection.php

---
+++ src/RelatedCollection.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Коллекция моделей связанных с моделью.
+ * @package evas-php\evas-orm
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Orm;
+
+use Evas\Base\Help\Collection;
+use Evas\Orm\ActiveRecord;
+use Evas\Orm\Relation;
+
+class RelatedCollection extends Collection
+{
+    /** @var ActiveRecord модель */
+    protected $model;
+    /** @var Relation связь */
+    protected $relation;
+    /** @var array значения первичных ключей моделей коллекции */
+    protected $ids = [];
+
+    /**
+     * Получение текущей модели коллекции.
+     * @return ActiveRecord|null модель
+     */
+    public function current(): ?ActiveRecord
+    {
+        return parent::current();
+    }
+
+    /**
+     * Проверка наличия модели в коллекции.
+     * @param int|string значение первичного ключа
+     * @return bool
+     */
+    public function has(int $id): bool
+    {
+        return in_array($id, $this->ids);
+    }
+
+    /**
+     * Добавление элемента в коллекцию.
+     * @param mixed элемент
+     * @return self
+     */
+    public function add($item)
+    {
+        if (!($item instanceof $this->relation->foreignModel)) {
+            if ($item instanceof ActiveRecord) $item = $item->toArray();
+            $item = new $this->relation->foreignModel($item);
+        }
+        if (!($id = $item->primaryValue()) || !$this->has($id)) {
+            $this->ids[] = $id;
+            return parent::add($item);
+        }
+        return $this;
+    }
+
+    /**
+     * Конструктор.
+     * @param ActiveRecord модель
+     * @param Relation связь
+     * @param array|Collection|null элементы для коллекции
+     */
+    public function __construct(ActiveRecord $model, Relation $relation, $items = null)
+    {
+        $this->model = &$model;
+        $this->relation = $relation;
+        parent::__construct($items);
+    }
+
+    /**
+     * Перезапрос связанных записей.
+     * @return self
+     */
+    public function reload()
+    {
+        $this->items = $this->relation->foreignModel::where(
+            $this->relation->foreignKey, 
+            $this->model->{$this->relation->localKey}
+        )->get();
+        return $this;
+    }
+
+    /**
+     * Сохраенение связанных записей.
+     * @return self
+     */
+    public function save()
+    {
+        foreach ($this->items as &$item) {
+            $item->save();
+        }
+        return $this;
+    }
+}
