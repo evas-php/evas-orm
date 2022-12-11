4ebcc14a2556cb5c057b7f2889c63c1f3ca37329
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-12-11 23:54:52 +0300

    Update QueryBuilderRelationsTrait.php

--- src/Traits/QueryBuilderRelationsTrait.php
+++ src/Traits/QueryBuilderRelationsTrait.php
@@ -8,160 +8,41 @@ namespace Evas\Orm\Traits;
 
 use Evas\Orm\ActiveRecord;
 use Evas\Orm\Relation;
+use Evas\Orm\Traits\QueryBuilderRelationsHasTrait;
+use Evas\Orm\Traits\QueryBuilderRelationsWithTrait;
 
 trait QueryBuilderRelationsTrait
 {
+    use QueryBuilderRelationsHasTrait;
+    use QueryBuilderRelationsWithTrait;
+
     /** @var string разделитель для полей связанных записей */
     public static $relationDataSeparator = '_-_';
-
-    protected $withs = [];
-    protected $withsAfter = [];
 
     protected function getRelation(string $name): ?Relation
     {
         return $this->model::getRelation($name);
     }
 
-    protected function addWith(string $name, $columns, self $query = null)
-    {
-        $relation = $this->getRelation($name);
-        if ($relation) {
-            if ($relation->isOne()) $list = &$this->withs;
-            else $list = &$this->withsAfter;
-            // if (!isset($list[$relation->name])) {
-            //     $list[$relation->name] = [$relation, $columns, $query];
-            // }
-            $list[$relation->name] = [$relation, $columns, $query];
-        }
-        // echo title('addWith: ' . $name, 3);// . dumpOrm($relation);
-        // echo dumpOrm($this->model::$relations);
-        return $this;
-    }
-
-    public function with(...$props)
-    {
-        foreach ($props as &$prop) {
-            if (is_string($prop)) {
-                @[$name, $columns] = explode(':', $prop);
-                $this->addWith($name, $columns);
-
-            } else if (is_array($prop)) {
-                foreach ($prop as $name => &$sub) {
-                    $columns = null;
-                    $query = null;
-
-                    if (is_string($name)) {
-                        if (is_string($sub) || is_array($sub)) {
-                            $columns = $sub;
-                        } else if (is_callable($sub)) {
-                            @[$name, $columns] = explode(':', $name);
-                            $relation = $this->getRelation($name);
-                            if (!$relation) return;
-                            $query = new static($this->db, $relation->foreignModel);
-                            $sub($query);
-                        }
-
-                    } else if (is_string($sub)) {
-                        @[$name, $columns] = explode(':', $sub);
-                    } else {
-                        throw new \InvalidArgumentException(sprintf(
-                            'Incorrect arguments in method %s()',
-                            __METHOD__
-                        ));
-                    }
-                    $this->addWith($name, $columns, $query);
-                }
-            }
-        }
-        return $this;
-    }
-
-    protected function addHas(
-        bool $isNot, bool $isWith, string $relationName, 
-        $operator = null, $value = null
-    ) {
-        @[$relationName, $columns] = explode(':', $relationName);
-        $relation = $this->getRelation($relationName);
-        if ($relation) {
-            $this->select(static::prepareModelColumns($this->columns, $this->model));
-            if ($columns) {
-                $columns = explode(',', $columns);
-                foreach ($columns as $column) {
-                    $this->whereNotNull($relation->foreignColumn($column, true));
-                }
-            }
-            if (func_num_args() > 3) {
-                if ($this->isQueryable($operator)) {
-                    if ($operator instanceof \Closure) {
-                        call_user_func($operator, $operator = $this->newQuery());
-                    }
-                    foreach ($operator->wheres as $where) {
-                        if (isset($where['columns'])) 
-                            foreach ($where['columns'] as &$column) {
-                                $this->addColumnTablePrefix($column, $relationName);
-                            }
-                        if (isset($where['column'])) 
-                            $where['column'] = $this->addColumnTablePrefix(
-                                $where['column'], $relationName
-                            );
-                        $this->pushWhere($where['type'], $where);
-                    }
-                } else {
-                    $this->prepareValueAndOperator(
-                        $value, $operator, func_num_args() === 4
-                    );
-                    $foreignPrimary = $relation->foreignPrimary(true);
-                    if ($columns) {
-                        // isHas
-                        $this->where(
-                            $relation->foreignColumn($column, true), 
-                            $operator, $value
-                        );
-                    } else {
-                        $this->count($foreignPrimary);
-                        $this->havingAggregate(
-                            'count', $foreignPrimary, $operator, $value
-                        );
-                        if ($value === 0) $isNot = true;
-                    }
-                }
-            }
-
-            $args = $relation->joinArgs(null, false);
-
-            if ($isNot) {
-                if (func_num_args() > 3) {
-                    $this->leftJoinSub(...$args);
-                    $this->groupBy($relation->localKey(true));
-                } else {
-                    $this->leftOuterJoinSub(...$args);
-                    $this->whereNull($relation->foreignKey());
-                }
-            } else {
-                $this->joinSub(...$args);
-                $this->groupBy($relation->localKey(true));
-            }
-        }
-
-        return $this;
-    }
-
-    public function has(string $relationName, $operator = null, $value = null)
-    {
-        return $this->addHas(false, false, ...func_get_args());
-    }
-
-    public function notHas(string $relationName, $operator = null, $value = null)
-    {
-        return $this->addHas(true, false, ...func_get_args());
-    }
-
 
     protected static function prepareColumns($columns): ?array
     {
-        return (empty($columns) || '*' == $columns
-        || (is_array($columns) && in_array('*', $columns))) 
-        ? null : explode(',', str_replace(' ', '', $columns));
+        // if (empty($columns) || '*' == $columns) {
+        //     return null;
+        // }
+        // if (is_array($columns)) {
+        //     if (in_array('*', $columns)) return null;
+        //     else return $columns;
+        // }
+
+        return (empty($columns) || '*' === $columns) ? null : (
+            is_array($columns) ? (in_array('*', $columns) ? null : $columns)
+            : explode(',', str_replace(' ', '', $columns))
+        );
+
+        // return (empty($columns) || '*' == $columns
+        // || (is_array($columns) && in_array('*', $columns))) 
+        // ? null : explode(',', str_replace(' ', '', $columns));
     }
 
     protected static function prepareModelColumns(
@@ -193,50 +74,38 @@ trait QueryBuilderRelationsTrait
         return $column;
     }
 
-    protected function applyWiths()
+    protected function applyRelationsBefore()
     {
-        // $withs = array_filter($this->withs, fn($with) => $with->isOne());
-        if (1 > count($this->withs)) return;
-        // echo title('applyWiths', 3);
-        $columns = static::prepareModelColumns($this->columns, $this->model);
-        $this->select($columns);
-        foreach ($this->withs as [$relation, $columns, $query]) {
-            $this->applyWith($relation, $columns, $query);
-        }
+        // // if (1 > count($this->withOne) && 1 > count($this->has)) return;
+        // if (1 > count($this->withs) && 1 > count($this->has)) return;
+        // $columns = static::prepareModelColumns($this->columns, $this->model);
+        // $this->select($columns);
+        // $this->applyWiths();
+        // $this->applyHases();
     }
 
-    protected function applyWith(
-        Relation $relation, $columns = null, self $query = null
-    ) {
-        $columns = static::prepareModelColumns(
-            $columns, $relation->foreignModel, $relation->name
-        );
-        $this->select($columns);
-        // $this->leftJoinSub(
-        //     $query ?? $relation->foreignTable, 
-        //     $relation->name, 
-        //     "{$relation->name}.{$relation->foreignKey}", 
-        //     $relation->localKey(true)
-        // );
-        $this->leftJoinSub(...$relation->joinArgs($query, true));
-        return $this;
-    }
-
-    protected function parseWiths(ActiveRecord &$model)
+    protected function applyRelationsAfter(array $ids, array &$models)
     {
         if (1 > count($this->withs)) return;
-        // echo title('parseWiths', 3) . dumpOrm($model);
-        $foreigns = [];
-        foreach ($model->toArray() as $key => $value) {
-            @[$fname, $fkey] = explode(static::$relationDataSeparator, $key, 2);
-            if ($fkey && in_array($fname, array_keys($this->withs))) {
-                $foreigns[$fname][$fkey] = $value;
-                unset($model->$key);
-            }
-        }
-        foreach ($foreigns as $fname => $subs) {
-            // $this->withs[$fname][0]->addRelated($result, $subs);
-            $model->addRelatedData($fname, $subs);
-        }
+        $this->applyWiths($ids, $models);
+        // if (1 > count($this->withMany)) return;
+        // foreach ($this->withMany as [$relation, $columns, $query]) {
+        //     $qb = (new static($this->db, $relation->foreignModel));
+        //     $subModels = $qb->whereIn($relation->foreignKey, $ids)->get();
+        //     if (!$subModels) continue;
+        //     $ids = [];
+        //     foreach ($subModels as $subModel) {
+        //         $ids[] = $subModel->primaryValue();
+        //         foreach ($models as $model) {
+        //             if ($model->{$relation->localKey} == $subModel->{$relation->foreignKey}) {
+        //                 $model->addRelated($relation->name, $subModel);
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     if (0 < count($ids)) {
+        //         $this->applyRelationsAfter($ids, $subModels);
+        //     }
+        // }
     }
 }
